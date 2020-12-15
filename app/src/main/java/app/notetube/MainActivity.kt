e6cceeb28bdfe5ac8c188ed7426172269af70c2a
Kyle <kkroberts1635@gmail.com>
2020-12-15 14:48:02 -0500

    Added search to main activity

--- app/src/main/java/app/notetube/MainActivity.kt
+++ app/src/main/java/app/notetube/MainActivity.kt
@@ -1,28 +1,32 @@
 package app.notetube
 
+import android.R.id
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ListView
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
+import android.widget.SearchView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import app.notetube.adapters.DocumentListAdapter
 import app.notetube.models.api.Document
 import app.notetube.models.api.User
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.json.JSONObject
 import java.lang.reflect.Type
 
+
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,7 +78,9 @@ class MainActivity : AppCompatActivity() {
         // Click listview item
         documentListView.setOnItemClickListener { parent, view, position, id ->
             val intent = Intent(this, notes_document::class.java)
-            intent.putExtra("VIDEO_DOCUMENT", currUser?.documents?.get(position))
+            var selectedDocument : Document = parent.adapter.getItem(position) as Document
+            //intent.putExtra("VIDEO_DOCUMENT", currUser?.documents?.get(position))
+            intent.putExtra("VIDEO_DOCUMENT", selectedDocument)
             startActivity(intent)
         }
 
@@ -94,6 +100,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.main_menu, menu)
 
+        val search : MenuItem? = menu?.findItem(R.id.app_bar_search)
+        val searchView : SearchView = search?.actionView as SearchView
+        searchView.queryHint = "Find document"
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                var adapter : DocumentListAdapter = documentListView.adapter as DocumentListAdapter
+                if (newText != null) {
+                    adapter.filter(newText)
+                }
+                return true
+            }
+        })
 
         return true
     }
